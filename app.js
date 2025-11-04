9507ca903cd9d1e7aeebd4e1ab5ae18c94865f7c
Cheila <61353903+shay2025@users.noreply.github.com>
2025-11-04 10:11:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
 require('./mongoose-db');
 require('./typeorm-db')
 
+const query = new URLSearchParams(window.location.search).get('query');
+document.getElementById('search_query').innerHTML = query;
+
 var st = require('st');
 var crypto = require('crypto');
 var express = require('express');
